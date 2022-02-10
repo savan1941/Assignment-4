a48ee196ce54b1349fa40aa77b36428e3284196c
savan1941 <97402042+savan1941@users.noreply.github.com>
2022-02-10 22:43:52 +0530

    ass-4

--- dashboard.js
+++ dashboard.js
@@ -56,11 +56,12 @@ function backfun() {
   console.log(datas.records[0].fields.email);
   var airtable = document.getElementById("airtable");
   console.log(airtable);
+  
   for (let i of datas.records) {
-    console.log(i.fields.name);
+   
     var tblrow = document.createElement("tr");
-    console.log(tblrow);
-    tblrow.innerHTML = ` <td>${i.fields.number}</td>
+    
+    tblrow.innerHTML = ` <td >${i.fields.number}</td>
                     <td>${i.fields.name}</td>
                     <td>${i.fields.email}</td>
                     <td>${i.fields.phone}</td>
@@ -71,3 +72,61 @@ function backfun() {
     airtable.appendChild(tblrow);
   }
 })();
+
+var uname = document.getElementById("uname");
+var email = document.getElementById("email");
+var pwd = document.getElementById("pwd");
+var phnnumber =  document.getElementById("phnnumber");
+var city = document.getElementById("city");
+var country = document.getElementById("country");
+var count = 5;
+
+var addform = document.getElementById("model-content");
+console.log(addform);
+
+addform.addEventListener("submit", saveinb);
+  
+  async function saveinb(e){
+    e.preventDefault();
+     console.log("savan");
+    let response = await fetch('https://api.airtable.com/v0/appPWO0x1lu3RrKgY/Table%201', {
+      method: 'POST',
+      headers: {
+          "Authorization": "Bearer keyubAggt3UoesLi4",
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          'fields':{
+              "number":count,
+              "phone": phnnumber.value,
+              "email": email.value,
+              "country": country.value,
+              "name": uname.value,
+              "city": city.value
+          }
+          
+      }) 
+  }).then(res => res.json()).then(data => console.log(data));
+  count++;
+ 
+  
+  uname.value = "";
+  email.value = "";
+  phnnumber.value = "";
+  city.value = "";
+  country.value = "";
+  editmodal[0].style.display = 'none';
+  // mainmain.style.opacity = "1";
+  location.reload();
+}  
+  
+//  let data =  fetch("https://api.airtable.com/v0/appPWO0x1lu3RrKgY/Table%201",{
+//     method: 'POST',
+//     headers: {
+//         "Authorization": "Bearer keyubAggt3UoesLi4",
+//         'Content-Type': 'application/json'
+//   }
+
+  
+
+// });
