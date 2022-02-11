1c8493ebd6c3f47f10292069d8e7c80fef5c4721
savan1941 <savan.sangani@marutitech.com>
2022-02-11 18:05:07 +0530

    sorting completed

--- dashboard.js
+++ dashboard.js
@@ -2,6 +2,10 @@ let modal = document.getElementsByClassName("modal");
 let container = document.getElementById("container");
 let cancel = document.getElementById("cancel");
 let editcancel = document.getElementById("editcancel");
+var airtable = document.getElementById("airtable");
+let serial = 1,
+  clicked;
+const entries = [];
 
 // Get the button that opens the modal
 var btn = document.getElementById("addagency");
@@ -52,22 +56,494 @@ function backfun() {
     }
   ).then((response) => response.json());
   // .then((data) => console.log(data.records[0].fields));
-  console.log(datas);
-  console.log(datas.records[0].fields.email);
-  var airtable = document.getElementById("airtable");
+
   console.log(airtable);
-  for (let i of datas.records) {
-    console.log(i.fields.name);
+
+  datas.records.forEach(function (i) {
+    var single = {
+      number: serial,
+      id: i.id,
+      uname: i.fields.name,
+      city: i.fields.city,
+      country: i.fields.country,
+      phnnumber: i.fields.phone,
+      email: i.fields.email,
+    };
+    serial++;
+    // console.log(i.id);
+    entries.push(single);
+
     var tblrow = document.createElement("tr");
-    console.log(tblrow);
-    tblrow.innerHTML = ` <td>${i.fields.number}</td>
+
+    tblrow.innerHTML = ` <td >${i.fields.number}</td>
                     <td>${i.fields.name}</td>
                     <td>${i.fields.email}</td>
                     <td>${i.fields.phone}</td>
                     <td>${i.fields.city}</td>
                     <td>${i.fields.country}</td>
-                    <td><button id="edit" onclick="myfunc1()">Edit</button>
-                    <button id="delete">Delete</button></td>`;
+                    <td><button id="edit" class="updatebtn" onclick="myfunc1()">Edit</button>
+                    </td>`;
     airtable.appendChild(tblrow);
-  }
+  });
 })();
+
+var uname = document.getElementById("uname");
+var email = document.getElementById("email");
+var pwd = document.getElementById("pwd");
+var phnnumber = document.getElementById("phnnumber");
+var city = document.getElementById("city");
+var country = document.getElementById("country");
+var count = 5;
+
+var addform = document.getElementById("model-content");
+console.log(addform);
+
+addform.addEventListener("submit", saveinb);
+
+async function saveinb(e) {
+  e.preventDefault();
+  console.log("savan");
+  let response = await fetch(
+    "https://api.airtable.com/v0/appPWO0x1lu3RrKgY/Table%201",
+    {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer keyubAggt3UoesLi4",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fields: {
+          number: serial,
+          phone: phnnumber.value,
+          email: email.value,
+          country: country.value,
+          name: uname.value,
+          city: city.value,
+        },
+      }),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  serial++;
+
+  uname.value = "";
+  email.value = "";
+  phnnumber.value = "";
+  city.value = "";
+  country.value = "";
+  editmodal[0].style.display = "none";
+  // mainmain.style.opacity = "1";
+  location.reload();
+}
+
+//edit form
+
+var ediform = document.getElementById("edit-content");
+
+window.onclick = function (e) {
+  if (e.target.classList.contains("updatebtn")) {
+    let par = e.target.parentElement.parentElement.childNodes;
+    clicked = Number(par[1].innerText);
+    console.log(clicked);
+
+    const phone = document.getElementById("editphnnumber");
+    phone.value = par[7].innerText;
+
+    const email = document.getElementById("editemail");
+    email.value = entries[clicked - 1].email;
+    console.log(email.value);
+
+    const city = document.getElementById("editcity");
+    city.value = par[9].innerText;
+    console.log(city.value);
+
+    const country = document.getElementById("editcountry");
+    country.value = par[11].innerText;
+    console.log(country.value);
+
+    const name = document.getElementById("editname");
+    name.value = par[3].innerText;
+    console.log(name.value);
+  }
+};
+
+editform.addEventListener("submit", updateinb);
+
+async function updateinb(e) {
+  console.log("ssss");
+  const editname = document.getElementById("editname");
+  const editemail = document.getElementById("editemail");
+  const editphnnumber = document.getElementById("editphnnumber");
+  const editcity = document.getElementById("editcity");
+  const editcountry = document.getElementById("editcountry");
+  // console.log(entries[clicked - 1].id);
+
+  e.preventDefault();
+  let response = await fetch(
+    `https://api.airtable.com/v0/appPWO0x1lu3RrKgY/Table%201/${
+      entries[clicked - 1].id
+    }`,
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: "Bearer keyubAggt3UoesLi4",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fields: {
+          number: entries[clicked - 1].number,
+          phone: editphnnumber.value,
+          email: editemail.value,
+          country: editcountry.value,
+          name: editname.value,
+          city: editcity.value,
+        },
+      }),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  editmodal[0].style.display = "none";
+  // mainmain.style.opacity = "1";
+  location.reload();
+}
+
+// sort by country
+
+function sortbycountry() {
+  let scountry = document.getElementById("scountry");
+
+  let child = airtable.childNodes;
+
+  for (let i of child) {
+    if (i.nodeName === "TR") {
+      // scountry.classList.remove("show");
+      i.style.display = "none";
+    }
+  }
+
+  if (scountry.classList.contains("show")) {
+    scountry.innerHTML = `<i class="fas fa-arrow-down"></i>`;
+
+    entries.sort((a, b) => {
+      if (a.country > b.country) return 1;
+      else if (a.country < b.country) return -1;
+      else return 0;
+    });
+
+    for (let j = 0; j < entries.length; j++) {
+      const tr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      td1.innerText = entries[j].number;
+      tr.appendChild(td1);
+
+      const td2 = document.createElement("td");
+      td2.innerText = entries[j].uname;
+      tr.appendChild(td2);
+
+      const td3 = document.createElement("td");
+      td3.innerText = entries[j].email;
+      tr.appendChild(td3);
+
+      const td4 = document.createElement("td");
+      td4.innerText = entries[j].phnnumber;
+      tr.appendChild(td4);
+
+      const td5 = document.createElement("td");
+      td5.innerText = entries[j].city;
+      tr.appendChild(td5);
+
+      const td6 = document.createElement("td");
+      td6.innerText = entries[j].country;
+      tr.appendChild(td6);
+
+      const td7 = document.createElement("td");
+      const td8 = document.createElement("button");
+      td6.setAttribute("class", "btn");
+      // td6.onclick = function() {editRow(item);};
+
+      td8.innerText = "Edit";
+      td7.appendChild(td8);
+      tr.appendChild(td7);
+
+      airtable.appendChild(tr);
+    }
+
+    scountry.classList.remove("show");
+  } else {
+    scountry.innerHTML = `<i class="fas fa-arrow-up"></i>`;
+
+    entries.sort((a, b) => {
+      if (a.country > b.country) return -1;
+      else if (a.country < b.country) return 1;
+      else return 0;
+    });
+
+    for (let j = 0; j < entries.length; j++) {
+      const tr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      td1.innerText = entries[j].number;
+      tr.appendChild(td1);
+
+      const td2 = document.createElement("td");
+      td2.innerText = entries[j].uname;
+      tr.appendChild(td2);
+
+      const td3 = document.createElement("td");
+      td3.innerText = entries[j].email;
+      tr.appendChild(td3);
+
+      const td4 = document.createElement("td");
+      td4.innerText = entries[j].phnnumber;
+      tr.appendChild(td4);
+
+      const td5 = document.createElement("td");
+      td5.innerText = entries[j].city;
+      tr.appendChild(td5);
+
+      const td6 = document.createElement("td");
+      td6.innerText = entries[j].country;
+      tr.appendChild(td6);
+
+      const td7 = document.createElement("td");
+      const td8 = document.createElement("button");
+      td6.setAttribute("class", "btn");
+      // td6.onclick = function() {editRow(item);};
+
+      td8.innerText = "Edit";
+      td7.appendChild(td8);
+      tr.appendChild(td7);
+
+      airtable.appendChild(tr);
+    }
+
+    scountry.classList.add("show");
+  }
+}
+
+//sort by name
+function sortbycity() {
+  let scity = document.getElementById("scity");
+
+  let child = airtable.childNodes;
+
+  for (let i of child) {
+    if (i.nodeName === "TR") {
+      // scountry.classList.remove("show");
+      i.style.display = "none";
+    }
+  }
+
+  if (scity.classList.contains("show1")) {
+    scity.innerHTML = `<i class="fas fa-arrow-down"></i>`;
+
+    entries.sort((a, b) => {
+      if (a.city > b.city) return 1;
+      else if (a.city < b.city) return -1;
+      else return 0;
+    });
+
+    for (let j = 0; j < entries.length; j++) {
+      const tr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      td1.innerText = entries[j].number;
+      tr.appendChild(td1);
+
+      const td2 = document.createElement("td");
+      td2.innerText = entries[j].uname;
+      tr.appendChild(td2);
+
+      const td3 = document.createElement("td");
+      td3.innerText = entries[j].email;
+      tr.appendChild(td3);
+
+      const td4 = document.createElement("td");
+      td4.innerText = entries[j].phnnumber;
+      tr.appendChild(td4);
+
+      const td5 = document.createElement("td");
+      td5.innerText = entries[j].city;
+      tr.appendChild(td5);
+
+      const td6 = document.createElement("td");
+      td6.innerText = entries[j].country;
+      tr.appendChild(td6);
+
+      const td7 = document.createElement("td");
+      const td8 = document.createElement("button");
+      td6.setAttribute("class", "btn");
+      // td6.onclick = function() {editRow(item);};
+
+      td8.innerText = "Edit";
+      td7.appendChild(td8);
+      tr.appendChild(td7);
+
+      airtable.appendChild(tr);
+    }
+
+    scity.classList.remove("show1");
+  } else {
+    scity.innerHTML = `<i class="fas fa-arrow-up"></i>`;
+
+    entries.sort((a, b) => {
+      if (a.city > b.city) return -1;
+      else if (a.city < b.city) return 1;
+      else return 0;
+    });
+
+    for (let j = 0; j < entries.length; j++) {
+      const tr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      td1.innerText = entries[j].number;
+      tr.appendChild(td1);
+
+      const td2 = document.createElement("td");
+      td2.innerText = entries[j].uname;
+      tr.appendChild(td2);
+
+      const td3 = document.createElement("td");
+      td3.innerText = entries[j].email;
+      tr.appendChild(td3);
+
+      const td4 = document.createElement("td");
+      td4.innerText = entries[j].phnnumber;
+      tr.appendChild(td4);
+
+      const td5 = document.createElement("td");
+      td5.innerText = entries[j].city;
+      tr.appendChild(td5);
+
+      const td6 = document.createElement("td");
+      td6.innerText = entries[j].country;
+      tr.appendChild(td6);
+
+      const td7 = document.createElement("td");
+      const td8 = document.createElement("button");
+      td6.setAttribute("class", "btn");
+      // td6.onclick = function() {editRow(item);};
+
+      td8.innerText = "Edit";
+      td7.appendChild(td8);
+      tr.appendChild(td7);
+
+      airtable.appendChild(tr);
+    }
+
+    scity.classList.add("show1");
+  }
+}
+
+//sort by name
+
+function sortbyname() {
+  let sname = document.getElementById("sname");
+  console.log(sname);
+
+  let child = airtable.childNodes;
+
+  for (let i of child) {
+    if (i.nodeName === "TR") {
+      // scountry.classList.remove("show");
+      i.style.display = "none";
+    }
+  }
+
+  if (sname.classList.contains("show3")) {
+    sname.innerHTML = `<i class="fas fa-arrow-down"></i>`;
+
+    entries.sort((a, b) => {
+      if (a.uname > b.uname) return 1;
+      else if (a.uname < b.uname) return -1;
+      else return 0;
+    });
+
+    for (let j = 0; j < entries.length; j++) {
+      const tr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      td1.innerText = entries[j].number;
+      tr.appendChild(td1);
+
+      const td2 = document.createElement("td");
+      td2.innerText = entries[j].uname;
+      tr.appendChild(td2);
+
+      const td3 = document.createElement("td");
+      td3.innerText = entries[j].email;
+      tr.appendChild(td3);
+
+      const td4 = document.createElement("td");
+      td4.innerText = entries[j].phnnumber;
+      tr.appendChild(td4);
+
+      const td5 = document.createElement("td");
+      td5.innerText = entries[j].city;
+      tr.appendChild(td5);
+
+      const td6 = document.createElement("td");
+      td6.innerText = entries[j].country;
+      tr.appendChild(td6);
+
+      const td7 = document.createElement("td");
+      const td8 = document.createElement("button");
+      td6.setAttribute("class", "btn");
+      // td6.onclick = function() {editRow(item);};
+
+      td8.innerText = "Edit";
+      td7.appendChild(td8);
+      tr.appendChild(td7);
+
+      airtable.appendChild(tr);
+    }
+
+    sname.classList.remove("show3");
+  } else {
+    sname.innerHTML = `<i class="fas fa-arrow-up"></i>`;
+
+    entries.sort((a, b) => {
+      if (a.uname > b.uname) return -1;
+      else if (a.uname < b.uname) return 1;
+      else return 0;
+    });
+
+    for (let j = 0; j < entries.length; j++) {
+      const tr = document.createElement("tr");
+      const td1 = document.createElement("td");
+      td1.innerText = entries[j].number;
+      tr.appendChild(td1);
+
+      const td2 = document.createElement("td");
+      td2.innerText = entries[j].uname;
+      tr.appendChild(td2);
+
+      const td3 = document.createElement("td");
+      td3.innerText = entries[j].email;
+      tr.appendChild(td3);
+
+      const td4 = document.createElement("td");
+      td4.innerText = entries[j].phnnumber;
+      tr.appendChild(td4);
+
+      const td5 = document.createElement("td");
+      td5.innerText = entries[j].city;
+      tr.appendChild(td5);
+
+      const td6 = document.createElement("td");
+      td6.innerText = entries[j].country;
+      tr.appendChild(td6);
+
+      const td7 = document.createElement("td");
+      const td8 = document.createElement("button");
+      td6.setAttribute("class", "btn");
+      // td6.onclick = function() {editRow(item);};
+
+      td8.innerText = "Edit";
+      td7.appendChild(td8);
+      tr.appendChild(td7);
+
+      airtable.appendChild(tr);
+    }
+
+    sname.classList.add("show3");
+  }
+}
